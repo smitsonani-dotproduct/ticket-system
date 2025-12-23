b60d18ac4d298b3190fa3940737e28432014bc7b
Smit S <smit.s@dotproduct.in>
2025-12-23 18:58:43 +0530

    minor improvements

---
+++ tickets/__init__.py
@@ -0,0 +1,1 @@
+print("From init (ticket app)")
--- tickets/urls.py
+++ tickets/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StationViewSet, TicketViewSet, UserViewSet
-from . import views
+from .views import StationViewSet, TicketViewSet, UserViewSet, calculate_price
+
+# from . import views
 
 router = DefaultRouter()
-router.register(r"users", UserViewSet)
-router.register(r"stations", StationViewSet)
+router.register(r"users", UserViewSet, basename="user")
+router.register(r"stations", StationViewSet, basename="station")
 router.register(r"tickets", TicketViewSet, basename="ticket")
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("calculate-price/", views.calculate_price),
+    path("calculate-price/", calculate_price),
 ]
 
 # # Station APIs
