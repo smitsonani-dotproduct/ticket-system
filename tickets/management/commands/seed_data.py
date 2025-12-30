9045695a3adad7e7f6dbf9ab270e6c28068aa601
Smit S <smit.s@dotproduct.in>
2025-12-30 16:29:12 +0530

    seed_data code enhancement: removed py ds

--- tickets/management/commands/seed_data.py
+++ tickets/management/commands/seed_data.py
@@ -14,28 +14,30 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS("Seeding started..."))
 
         # ---- Stations ----
-        station_names = set()
-        while len(station_names) < 5:
-            station_names.add(faker.city())
-
-        for name in station_names:
-            Station.objects.create(name=name)
+        for _ in range(5):
+            name = faker.city()
+            Station.objects.get_or_create(name=name)
 
         self.stdout.write(self.style.SUCCESS("Stations created"))
 
         # ---- Tickets ----
-        users = list(User.objects.all())
-        stations = list(Station.objects.all())
+        users_qs = User.objects.only("id")
+        users_count = users_qs.count()
+        stations_qs = Station.objects.only("id")
+        stations_count = stations_qs.count()
 
-        print("users =>", users)
-        print("stations =>", stations)
+        print("users =>", users_qs, users_count)
+        print("stations =>", stations_qs, stations_count)
 
         for _ in range(5):
-            from_station, to_station = random.sample(stations, 2)
+            from_station = stations_qs[random.randint(0, stations_count - 1)]
+            to_station = stations_qs.exclude(id=from_station.id)[
+                random.randint(0, stations_count - 2)
+            ]
             price = abs(from_station.id - to_station.id) + 100
 
-            Ticket.objects.create(
-                user=random.choice(users),
+            Ticket.objects.get_or_create(
+                user=users_qs[random.randint(0, users_count - 1)],
                 from_station=from_station,
                 to_station=to_station,
                 date=faker.date_this_year(),
